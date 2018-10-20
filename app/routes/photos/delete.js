e2cf7c91f60ab2eeb278351f32d50d3454075e90
Anne Fortuin <phortuin@gmail.com>
2018-10-20 23:09:03 +0200

    fix: delete from cloudinary if upload fails at mongo

--- app/routes/photos/delete.js
+++ app/routes/photos/delete.js
@@ -1,28 +1,10 @@
 const Photo = require('@models/photo')
-const cloudinary = require('cloudinary')
+const cloudinaryDestroy = require('@lib/cloudinary-destroy')
 
 module.exports = (req, res, next) => {
 	Photo.findById(req.params.id).exec()
-		.then(photo => {
-			let cloudinaryId = photo.url.substr(photo.url.lastIndexOf('/') + 1).slice(0, -4) // Get the last part of the public URL, without the file extension: http://res.cloudinary.com/phortuin/image/upload/v1540029529/AGJKE1685.JPG.jpg
-			return cloudinaryDestroy(cloudinaryId)
-		})
-		.then(result => {
-			console.log(result)
-			return Photo.findByIdAndRemove(req.params.id).exec() // messy, should just be photo.remove()
-		})
+		.then(photo => cloudinaryDestroy(photo.url))
+		.then(result => Photo.findByIdAndRemove(req.params.id).exec()) // @todo messy, should just be photo.remove()
 		.then(() => res.redirect('/photos'))
 		.catch(next)
-}
-
-function cloudinaryDestroy(id) {
-	return new Promise((resolve, reject) => {
-	    cloudinary.v2.uploader.destroy(id, function(error, result) {
-	        if (error) {
-	            reject(error)
-	        } else {
-	            resolve(result)
-	        }
-	    })
-	})
 }
--- app/routes/photos/post.js
+++ app/routes/photos/post.js
@@ -1,18 +1,30 @@
 const moment = require('moment')
 const Photo = require('@models/photo')
 const cloudinaryUpload = require('@lib/cloudinary-upload')
+const cloudinaryDestroy = require('@lib/cloudinary-destroy')
 const renderer = require('@lib/renderer')
 
 module.exports = (req, res, next) => {
+	let cloudinaryUrl
 	cloudinaryUpload(req.files.photo[0])
 		.then(({ url }) => {
 			let photo = new Photo()
 			photo.credit = req.body.credit
 			photo.caption = req.body.caption
 			photo.date = moment(req.body.date) // @todo Deprecation warning: value provided is not in a recognized RFC2822 or ISO format. moment construction falls back to js Date(), which is not reliable across all browsers and versions. Non RFC2822/ISO date formats are discouraged and will be removed in an upcoming major release. Please refer to http://momentjs.com/guides/#/warnings/js-date/ for more info.
-			photo.url = url
-			return photo.save()
+			photo.url = cloudinaryUrl = url
+			return photo.save() // @todo if this fails, photo should be deleted from cloudinary!!
 		})
 		.then(photo => res.send(renderer.render('views/thanks.html', photo.toObject()))) // @todo make redirect, to prevent uploading same photo multiple times
-		.catch(next)
+		.catch(err => {
+			cloudinaryDestroy(cloudinaryUrl)
+				.then(() => {
+					next(err)
+				})
+				.catch(cloudinaryErr => {
+					console.error('Multiple errors occurred, logging the last one first:')
+					console.error(cloudinaryErr)
+					next(err)
+				})
+		})
 }
