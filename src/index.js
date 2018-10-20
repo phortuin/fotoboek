7d360b926fd55fd841ed09935264777bba442f02
Anne Fortuin <phortuin@gmail.com>
2018-10-20 22:52:46 +0200

    feat: improve form usability by removing date field & adding image preview

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 	document.getElementById('photo').addEventListener('change', event => {
 		let files = [].slice.call(event.target.files)
 		files.forEach(file => {
-			let date = new Date(file.lastModifiedDate).toISOString()
-			document.getElementById('date').value = date.slice(0, -8) // remove seconds/milliseconds & timezone (:00.000Z) from ISO8601 date, see https://stackoverflow.com/a/28760255/554821 and https://stackoverflow.com/a/20114741/554821
+			let reader = new FileReader()
+			reader.onload = event => {
+				document.getElementById('photopreview').setAttribute('src', event.target.result)
+			}
+			reader.readAsDataURL(file)
+			document.getElementById('date').value = new Date(file.lastModifiedDate).toISOString()
 		})
 	})
 }
