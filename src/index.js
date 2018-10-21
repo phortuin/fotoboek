c3c2cf9c176f8c0fec0e56a6fc3f48da5551df65
Anne Fortuin <phortuin@gmail.com>
2018-10-21 20:39:33 +0200

    fix: prevent element not found error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 {
-	document.getElementById('photo').addEventListener('change', event => {
-		let files = [].slice.call(event.target.files)
-		files.forEach(file => {
-			let reader = new FileReader()
-			reader.onload = event => {
-				document.getElementById('photopreview').setAttribute('src', event.target.result)
-			}
-			reader.readAsDataURL(file)
-			document.getElementById('date').value = new Date(file.lastModifiedDate).toISOString()
+	let fileInput = document.getElementById('photo')
+	if (fileInput) {
+		fileInput.addEventListener('change', event => {
+			let files = [].slice.call(event.target.files)
+			files.forEach(file => {
+				let reader = new FileReader()
+				reader.onload = event => {
+					document.getElementById('photopreview').setAttribute('src', event.target.result)
+				}
+				reader.readAsDataURL(file)
+				document.getElementById('date').value = new Date(file.lastModifiedDate).toISOString()
+			})
 		})
-	})
+	}
 }
